001b5412c7f66c25a82ab1b1bfcd75fbbc4292bd
verma7jr <100682731+verma7jr@users.noreply.github.com>
2026-06-15 16:23:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
